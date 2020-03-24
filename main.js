4c8b4beb97c6e157ac70193f55fe68bb64fda002
Joao Moreno <joao.moreno@microsoft.com>
2020-03-24 11:21:46 +0100

    mobile

--- main.js
+++ main.js
@@ -30,6 +30,8 @@ const Timer = {
   }
 };
 
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
 class App {
 
   constructor() {
@@ -47,6 +49,10 @@ class App {
   }
 
   actionsView() {
+    if (isMobile) {
+      return [];
+    }
+
     if (this.state === 'idle') {
       return [
         this.recorded && m('button', { class: 'button primary', onclick: () => this.startRecording() }, [
@@ -100,7 +106,8 @@ class App {
         return [
           m('p', 'Create animated GIFs from a screen recording.'),
           m('p', 'Client-side only, no data is uploaded. Modern browser required.'),
-          m('button', { class: 'button primary', onclick: () => this.startRecording() }, [
+          isMobile ? m('p', 'Sorry, no mobile support.') : undefined,
+          isMobile ? undefined : m('button', { class: 'button primary', onclick: () => this.startRecording() }, [
             m('img', { src: 'https://icongr.am/octicons/play.svg?size=16&color=ffffff' }),
             'Start Recording'
           ])
