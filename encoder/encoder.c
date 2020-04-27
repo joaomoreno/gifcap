c00083ccb648038b1e7793da90536636e9b9b574
João Moreno <mail@joaomoreno.com>
2020-04-27 10:28:54 +0200

    animation!

--- encoder/encoder.c
+++ encoder/encoder.c
@@ -2,53 +2,92 @@
 #include <stdlib.h>
 #include <libimagequant.h>
 #include <lcdfgif/gif.h>
+#include <gifsicle.h>
 
 int main()
 {
   return 0;
 }
 
-unsigned int encode(void *raw_image_data, int width, int height)
+unsigned int encode(void *one, void *two, void *three, int width, int height)
 {
   liq_attr *attr = liq_attr_create();
-  liq_image *image = liq_image_create_rgba(attr, raw_image_data, width, height, 0);
+  liq_image *image_one = liq_image_create_rgba(attr, one, width, height, 0);
+  liq_image *image_two = liq_image_create_rgba(attr, two, width, height, 0);
+  liq_image *image_three = liq_image_create_rgba(attr, three, width, height, 0);
+
+  liq_histogram *histogram = liq_histogram_create(attr);
+  liq_histogram_add_image(histogram, attr, image_one);
+  liq_histogram_add_image(histogram, attr, image_two);
+  liq_histogram_add_image(histogram, attr, image_three);
+
   liq_result *res;
-  liq_error err = liq_image_quantize(image, attr, &res);
+  liq_error err = liq_histogram_quantize(histogram, attr, &res);
 
-  Gif_Image *gif_image = Gif_NewImage();
-  gif_image->width = width;
-  gif_image->height = height;
-  Gif_CreateUncompressedImage(gif_image, 0);
+  Gif_Image *gif_image_one = Gif_NewImage();
+  gif_image_one->width = width;
+  gif_image_one->height = height;
+  gif_image_one->delay = 100;
+  Gif_CreateUncompressedImage(gif_image_one, 0);
+  liq_write_remapped_image(res, image_one, gif_image_one->image_data, width * height);
 
-  liq_write_remapped_image(res, image, gif_image->image_data, width * height);
+  Gif_Image *gif_image_two = Gif_NewImage();
+  gif_image_two->width = width;
+  gif_image_two->height = height;
+  gif_image_two->delay = 100;
+  Gif_CreateUncompressedImage(gif_image_two, 0);
+  liq_write_remapped_image(res, image_two, gif_image_two->image_data, width * height);
+
+  Gif_Image *gif_image_three = Gif_NewImage();
+  gif_image_three->width = width;
+  gif_image_three->height = height;
+  gif_image_three->delay = 100;
+  Gif_CreateUncompressedImage(gif_image_three, 0);
+  liq_write_remapped_image(res, image_three, gif_image_three->image_data, width * height);
 
   const liq_palette *pal = liq_get_palette(res);
-
-  printf("ncolors: %d\n", pal->count);
-
-  Gif_Colormap *gif_colormap = Gif_NewFullColormap(pal->count, pal->count);
+  Gif_Colormap *colormap = Gif_NewFullColormap(pal->count, pal->count);
 
   for (int i = 0; i < pal->count; i++)
   {
     liq_color color = pal->entries[i];
-    gif_colormap->col[i].gfc_red = color.r;
-    gif_colormap->col[i].gfc_green = color.g;
-    gif_colormap->col[i].gfc_blue = color.b;
-    gif_colormap->col[i].haspixel = 0;
+    colormap->col[i].pixel = 256;
+    colormap->col[i].gfc_red = color.r;
+    colormap->col[i].gfc_green = color.g;
+    colormap->col[i].gfc_blue = color.b;
+    colormap->col[i].haspixel = 1;
   }
 
   liq_result_destroy(res);
-  liq_image_destroy(image);
+  liq_histogram_destroy(histogram);
+  liq_image_destroy(one);
+  liq_image_destroy(two);
+  liq_image_destroy(three);
   liq_attr_destroy(attr);
-
-  gif_image->local = gif_colormap;
-  gif_image->local->refcount = 1;
 
   Gif_Stream *gif_stream = Gif_NewStream();
   gif_stream->screen_width = width;
   gif_stream->screen_height = height;
+  gif_stream->global = colormap;
+  gif_stream->global->refcount = 1;
+  gif_stream->loopcount = 0;
 
-  Gif_AddImage(gif_stream, gif_image);
+  Gif_AddImage(gif_stream, gif_image_one);
+  Gif_AddImage(gif_stream, gif_image_two);
+  Gif_AddImage(gif_stream, gif_image_three);
+
+  Gt_Frame *gif_frame = Gif_New(Gt_Frame);
+  gif_frame->input_filename = "";
+  gif_frame->no_extensions = 1;
+  gif_frame->no_app_extensions = 1;
+  gif_frame->extensions = NULL;
+
+  // merge_image(gif_stream, NULL, gif_image, gif_frame, 0);
+
+  // do_colormap_change()
+  //   colormap_stream() -> unify color palettes
+  //  merge_and_write_frames()
+  //    merge_frame_interval()
 
   Gif_CompressInfo gif_write_info;
   Gif_InitCompressInfo(&gif_write_info);
@@ -58,9 +97,10 @@ unsigned int encode(void *raw_image_data, int width, int height)
   Gif_FullWriteFile(gif_stream, &gif_write_info, file);
   fclose(file);
 
+  Gif_Delete(gif_frame);
   Gif_Delete(gif_stream);
-  Gif_Delete(gif_image);
-  Gif_Delete(gif_colormap);
+  Gif_Delete(gif_image_one);
+  Gif_Delete(colormap);
 
-  return pal->count;
+  return 0;
 }
--- encoder/test/test.js
+++ encoder/test/test.js
@@ -1,27 +1,36 @@
 function main() {
   const encode = Module['_encode'];
   const canvas = document.getElementById('canvas');
-  const input = document.getElementById('input');
   const output = document.getElementById('output');
 
-  canvas.width = input.width;
-  canvas.height = input.height;
+  const images = ['one', 'two', 'three']
+    .map(id => document.getElementById(id));
+
+  canvas.width = images[0].width;
+  canvas.height = images[0].height;
 
   const ctx = canvas.getContext('2d');
-  ctx.drawImage(input, 0, 0);
+  const imageData = images.map(img => {
+    ctx.drawImage(img, 0, 0);
+    return ctx.getImageData(0, 0, canvas.width, canvas.height);
+  });
 
-  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  const byteLength = imageData.data.byteLength;
-  const ptr = Module._malloc(byteLength);
+  const ptrs = imageData.map(imageData => {
+    const length = imageData.data.byteLength;
+    const ptr = Module._malloc(length);
+    const buffer = new Uint8Array(Module.HEAPU8.buffer, ptr, length);
+    buffer.set(imageData.data);
+
+    return ptr;
+  });
 
   const start = Date.now();
-  const buffer = new Uint8Array(Module.HEAPU8.buffer, ptr, byteLength);
-  buffer.set(imageData.data);
-  encode(ptr, imageData.width, imageData.height);
+  encode(ptrs[0], ptrs[1], ptrs[2], canvas.width, canvas.height);
   console.log(`took ${Date.now() - start}ms`);
 
   const outputBuffer = FS.readFile('/output.gif');
   const blob = new Blob([outputBuffer], { type: 'image/gif' });
+  console.log(blob);
   const url = URL.createObjectURL(blob);
 
   output.src = url;
