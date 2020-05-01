35cb7f80c1c0ec499feb23ffd4e5cb218a120e98
João Moreno <mail@joaomoreno.com>
2020-05-01 19:26:59 +0200

    update test

--- encoder/test/test.js
+++ encoder/test/test.js
@@ -1,70 +1,96 @@
-function main() {
-  const canvas = document.getElementById('canvas');
+const _log = document.getElementById('log');
 
-  const images = ['one', 'two', 'three']
-    .map(id => document.getElementById(id));
-
-  canvas.width = images[0].width;
-  canvas.height = images[0].height;
-
-  const encoder = new GifEncoder({
-    width: canvas.width,
-    height: canvas.height
-  });
-
-  const ctx = canvas.getContext('2d');
-
-  const imageData = images.map(img => {
-    ctx.drawImage(img, 0, 0);
-    return ctx.getImageData(0, 0, canvas.width, canvas.height);
-  });
-
-  for (const img of imageData) {
-    encoder.addFrame(img, 1000);
-  }
-
-  const startGifsicle = Date.now();
-
-  encoder.on('progress', progress => {
-    console.log('progress', progress);
-  });
-
-  encoder.once('finished', blob => {
-    const stats = document.getElementById('stats');
-    stats.innerHTML += `<p>gifsicle: ${Date.now() - startGifsicle}ms, ${blob.size} bytes</p>`;
-
-    const url = URL.createObjectURL(blob);
-    const gifsicle = document.getElementById('gifsicle');
-    gifsicle.src = url;
-    const link = document.getElementById('gifsicle-link');
-    link.href = url;
-  });
-
-  encoder.render();
-
-  // JS GIF
-
-  // const startGifjs = Date.now();
-  // const gif = new GIF({
-  //   workers: 1,
-  //   quality: 10,
-  //   width: canvas.width,
-  //   height: canvas.height,
-  //   workerScript: '../../gif.worker.js',
-  // });
-
-  // gif.addFrame(imageData[0], { delay: 1000 });
-  // gif.addFrame(imageData[1], { delay: 1000 });
-  // gif.addFrame(imageData[2], { delay: 1000 });
-
-  // gif.once('finished', blob => {
-  //   stats.innerHTML += `<p>gifjs ${Date.now() - startGifjs}ms, ${blob.size} bytes</p>`;
-  //   const url = URL.createObjectURL(blob);
-  //   const gifjs = document.getElementById('gifjs');
-  //   gifjs.src = url;
-  // });
-
-  // gif.render();
+function log(str) {
+  _log.innerHTML += `${str}<br />`;
 }
 
-setTimeout(main, 100);
+function benchmarkGifsicle(imageData) {
+  return new Promise(c => {
+    const start = Date.now();
+    const encoder = new GifEncoder({
+      width: canvas.width,
+      height: canvas.height
+    });
+
+    for (const img of imageData) {
+      encoder.addFrame(img, 10);
+    }
+
+    encoder.once('finished', blob => {
+      log(`gifsicle: ${Date.now() - start}ms, ${Math.floor(blob.size / 1000)}Kb`);
+
+      const url = URL.createObjectURL(blob);
+      const gifsicle = document.getElementById('gifsicle');
+      gifsicle.src = url;
+      c();
+    });
+
+    encoder.render();
+  });
+}
+
+function benchmarkGifjs(imageData) {
+  return new Promise(c => {
+    const start = Date.now();
+    const gif = new GIF({
+      workers: 1,
+      quality: 10,
+      width: canvas.width,
+      height: canvas.height,
+      workerScript: '/encoder/test/gif.worker.js',
+    });
+
+    for (const img of imageData) {
+      gif.addFrame(img, { delay: 10 });
+    }
+
+    gif.once('finished', blob => {
+      log(`gifjs: ${Date.now() - start}ms, ${Math.floor(blob.size / 1000)}Kb`);
+
+      const url = URL.createObjectURL(blob);
+      const gifjs = document.getElementById('gifjs');
+      gifjs.src = url;
+      c();
+    });
+
+    gif.render();
+  });
+}
+
+async function benchmark(imageData1, imageData2) {
+  log('gifjs: rendering...');
+  await benchmarkGifjs(imageData1);
+  log('gifsicle: rendering...');
+  await benchmarkGifsicle(imageData2);
+}
+
+function main() {
+  const video = document.getElementById('video');
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  let frame = 0;
+  const imageData1 = [];
+  const imageData2 = [];
+
+  const next = () => {
+    if (frame === 30) {
+      benchmark(imageData1, imageData2);
+      return;
+    }
+
+    video.currentTime = frame++;
+  };
+
+  video.addEventListener('timeupdate', () => {
+    ctx.drawImage(video, 0, 0);
+    imageData1.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+    imageData2.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+    next();
+  });
+
+  log('capturing frames...');
+  next();
+}
+
+window.onload = main;
